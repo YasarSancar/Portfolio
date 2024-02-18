30184c2b8cd876763360909dcc5144d7dfd91653
YasarSancar <160413382+YasarSancar@users.noreply.github.com>
2024-02-18 19:37:56 +0100

    Add files via upload

---
+++ js/data.js
@@ -0,0 +1,61 @@
+const data = {
+    portfolios: [
+        {
+            techStack: "HTML, CSS, JS",
+            title: "Prison Dungeon",
+            githubLink: "#",
+            projectLink: "#",
+            imgLink: "images/portfolio/preview/Prison_Dungeon.webp",
+            hqLink: "images/portfolio/Prison_Dungeon.png",
+            panoLink: "images/portfolio/pano/Prison_Dungeon_Panorama.png",
+            viewport: {
+                minYaw: -45,
+                maxYaw: 45,
+                maxHfov: 75,
+                hfov: 65,
+                minPitch: -60,
+                maxPitch: 60,
+            }
+        },
+        {
+            techStack: "HTML, CSS, JS",
+            title: "Sunset Forest",
+            githubLink: "#",
+            projectLink: "#",
+            imgLink: "images/portfolio/preview/Sunset_Forest.webp",
+            hqLink: "images/portfolio/Sunset_Forest.png",
+            panoLink: "images/portfolio/pano/Sunset_Forest_Panorama.png"
+        },
+        {
+            techStack: "HTML, CSS, JS",
+            title: "Forest Day",
+            githubLink: "#",
+            projectLink: "#",
+            imgLink: "images/portfolio/preview/Forest_Day.webp",
+            hqLink: "images/portfolio/Forest_Day.png"
+        },
+        {
+            techStack: "HTML, CSS, JS",
+            title: "Campfire Night",
+            githubLink: "#",
+            projectLink: "#",
+            imgLink: "images/portfolio/preview/Campfire_Night.webp",
+            hqLink: "images/portfolio/Campfire_Night.png"
+        },
+        {
+            techStack: "HTML, CSS, JS",
+            title: "Crystal Cave",
+            githubLink: "#",
+            projectLink: "#",
+            imgLink: "images/portfolio/preview/Crystal_Cave.webp",
+            hqLink: "images/portfolio/Crystal_Cave.png"
+        },
+        {
+            techStack: "HTML, CSS, JS",
+            title: "Kieran Animation",
+            githubLink: "#",
+            projectLink: "#",
+            videoLink: "images/portfolio/Kieran_Anim.webm"
+        }
+    ]
+}
